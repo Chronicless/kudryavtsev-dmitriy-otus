5854675bcff928e5c90364a782ac1ea0aa1538ea
Dmitriy <chrono.xcrosss@gmail.com>
2019-03-31 14:17:24 +0300

    bugfix

--- javascript/1-sum/sum.js
+++ javascript/1-sum/sum.js
@@ -2,7 +2,7 @@ function sum(number) {
   let total = 0;
 
   function internalSum(nextNumber) {
-    if (nextNumber) {
+    if (typeof nextNumber !== 'undefined') {
       total += nextNumber;
       return internalSum;
     } else {
@@ -15,5 +15,5 @@ function sum(number) {
 
 }
 
-const isValid = (1 + 2 + 3 + 4 + 5 + 6) === sum(1)(2)(3)(4)(5)(6)();
+const isValid = (1 + 2 + 3 + 4 + 5 + 6 + 0 + 1) === sum(1)(2)(3)(4)(5)(6)(0)(1)();
 console.log(isValid);
