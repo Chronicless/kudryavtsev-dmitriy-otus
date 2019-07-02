13aca596f963178b9eb7b2736950f5a163004c2d
Кудрявцев Дмитрий Юрьевич <Kudryavtsev.D.Yu2@sberbank.ru>
2019-07-02 11:54:10 +0300

    fixes

--- javascript/4-fs_tree/index.js
+++ javascript/4-fs_tree/index.js
@@ -12,44 +12,44 @@ function getFolderData(path) {
   const childDirsDataPromises = [];
   return new Promise(async (resolve, reject) => {
     const dirData = await fsReadDir(path).catch((err) => { console.log(`Error occurred during reading path ${path} . Error : ${err.message}`); reject(); });
-    if (dirData.length > 0) {
-      // dir is not empty
-      // collecting info about files in current dir
-      const dirDataPromises = [];
-      for (let i = 0; i < dirData.length; i += 1) {
-        dirDataPromises.push(fsStat(`${path}/${dirData[i]}`));
-      }
-      const dirDataStats = await Promise.all(dirDataPromises).catch((err) => { console.log(`Failed to collect stats about files in dir ${path}.Error : ${err.message}`); reject(); });
-
-      // filling files and dirs arrays
-      for (let j = 0; j < dirDataStats.length; j += 1) {
-        if (dirDataStats[j].isDirectory()) {
-          // for dir making recursive call, dive deeper....
-          childDirsDataPromises.push(getFolderData(`${path}/${dirData[j]}`));
-        } else {
-          // is file
-          files.push(`${path}/${dirData[j]}`);
-        }
-      }
-
-      if (childDirsDataPromises.length === 0) {
-        // we dont have dirs on this level... returning
-        resolve({ dirs, files });
-      } else {
-        // we have dirs here ... waiting for async call with result for all dirs on this level
-        const childData = await Promise.all(childDirsDataPromises).catch((err) => { console.log(`Error occurred in recursive call. ${err.message}`); reject(); });
-
-        for (let a = 0; a < childData.length; a += 1) {
-          // combining this level data with recursive call data from deeper levels...
-          dirs = dirs.concat(childData[a].dirs);
-          files = files.concat(childData[a].files);
-        }
-        resolve({ dirs, files });
-      }
-    } else {
+    if (dirData.length === 0) {
       // folder is empty
       resolve({ files, dirs });
+      return;
     }
+    // dir is not empty
+    // collecting info about files in current dir
+    const dirDataPromises = [];
+    dirData.forEach((fsEntity) => {
+      dirDataPromises.push(fsStat(`${path}/${fsEntity}`));
+    });
+    const dirDataStats = await Promise.all(dirDataPromises).catch((err) => { console.log(`Failed to collect stats about files in dir ${path}.Error : ${err.message}`); reject(); });
+
+    // filling files and dirs arrays
+    for (let j = 0; j < dirDataStats.length; j += 1) {
+      if (dirDataStats[j].isDirectory()) {
+        // for dir making recursive call, dive deeper....
+        childDirsDataPromises.push(getFolderData(`${path}/${dirData[j]}`));
+      } else {
+        // is file
+        files.push(`${path}/${dirData[j]}`);
+      }
+    }
+
+    if (childDirsDataPromises.length === 0) {
+      // we dont have dirs on this level... returning
+      resolve({ dirs, files });
+      return;
+    }
+    // we have dirs here ... waiting for async call with result for all dirs on this level
+    const childData = await Promise.all(childDirsDataPromises).catch((err) => { console.log(`Error occurred in recursive call. ${err.message}`); reject(); });
+
+    childData.forEach((childObj) => {
+      // combining this level data with recursive call data from deeper levels...
+      dirs = dirs.concat(childObj.dirs);
+      files = files.concat(childObj.files);
+    });
+    resolve({ dirs, files });
   });
 }
 
